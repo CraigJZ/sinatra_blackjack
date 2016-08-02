75b872dabbb3f912648fb58929f290612f213959
Craig Zaccaro <czaccaro@live.com>
2016-08-02 19:55:21 -0400

    Set up initial game functions

--- main.rb
+++ main.rb
@@ -6,16 +6,34 @@ use Rack::Session::Cookie, :key => 'rack.session',
                            :secret => 'your_secret'
 
 get '/' do
-  "Hello World. This is some sample text"
+  if session[:player_name]
+    redirect '/game'
+  else
+    erb :new_player
+  end
 end
 
-get '/new_game' do #first screen, gets users' name
+post '/new_player' do
+  session[:player_name] = params[:player_name]
+  redirect '/game'
 end
 
 get '/bet' do
 end
 
 get '/game' do
+  suits = ['Heart', 'Club', 'Diamond', 'Spade']
+  card_values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+  session[:deck] = suits.product(card_values).shuffle!
+  session[:player_hand] = []
+  session[:dealer_hand] = []
+  session[:player_hand] << session[:deck].pop
+  session[:dealer_hand] << session[:deck].pop
+  session[:player_hand] << session[:deck].pop
+  session[:dealer_hand] << session[:deck].pop
+
+
+  erb :game
 end
 
 get '/game_over' do
