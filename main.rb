d8f16e89bee250d741fe5802c97327d82a213cb3
Craig Zaccaro <czaccaro@live.com>
2016-08-30 18:59:25 -0400

    Add Ajax functionality to hit and stay game functions

--- main.rb
+++ main.rb
@@ -51,20 +51,20 @@ helpers do
     @play_again = true
     @show_hit_or_stay_buttons = false
     session[:player_pot] = session[:player_pot] + session[:player_bet]
-    @success = "<strong>#{session[:player_name]} wins!</strong> #{msg}"
+    @winner = "<strong>#{session[:player_name]} wins!</strong> #{msg}"
   end
 
   def loser!(msg)
     @play_again = true
     @show_hit_or_stay_buttons = false
     session[:player_pot] = session[:player_pot] - session[:player_bet]
-    @error = "<strong>#{session[:player_name]} loses.</strong> #{msg}"
+    @loser = "<strong>#{session[:player_name]} loses.</strong> #{msg}"
   end
 
   def tie!(msg)
     @play_again = true
     @show_hit_or_stay_buttons = false
-    @success = "<strong>It's a tie!</strong> #{msg}"
+    @winner = "<strong>It's a tie!</strong> #{msg}"
   end
 end
 
@@ -139,7 +139,7 @@ post '/game/player/hit' do
   elsif player_total > BLACKJACK
     loser!("#{session[:player_name]} busted at #{player_total}.")
   end
-  erb :game
+  erb :game, layout: false
 end
 
 post '/game/player/stay' do
@@ -162,7 +162,7 @@ get '/game/dealer' do
   else
     @show_dealer_hit_button = true
   end
-  erb :game
+  erb :game, layout: false
 end
 
 post '/game/dealer/hit' do
@@ -183,7 +183,7 @@ get '/game/compare_hands' do
   else
     tie!("Both #{session[:player_name]} and the dealer stayed at #{player_total}.")
   end
-  erb :game
+  erb :game, layout: false
 end
 
 get '/game_over' do
